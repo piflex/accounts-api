6364496a8d4de2c275f2da35b87f4b7ff97626e2
Milos Tomic <milos.tomic@boutsourcing.com>
2014-05-30 09:22:50 +0200

    initial

---
+++ src/Appsco/Accounts/Api/Model/Directory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Appsco\Accounts\Api\Model;
+
+class Directory 
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+} 
---
+++ src/Appsco/Accounts/Api/Model/Profile.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Appsco\Accounts\Api\Model;
+
+class Profile 
+{
+    protected $id;
+
+    protected $email;
+
+    protected $firstName;
+
+    protected $lastName;
+
+    protected $locale;
+
+    protected $timezone;
+
+    protected $gender;
+
+    protected $country;
+
+    protected $province;
+
+    protected $city;
+
+    protected $phone;
+
+    protected $pictureUrl;
+
+    protected $directory;
+} 
